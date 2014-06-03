3cf95dff87e793b12e8daaadc3be2b1e4c02e295
drw86312 <david.warner4@gmail.com>
2014-06-03 13:17:39 -0500

    Initial Commit

---
+++ LostTests/LostTests.m
@@ -0,0 +1,34 @@
+//
+//  LostTests.m
+//  LostTests
+//
+//  Created by David Warner on 6/3/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LostTests : XCTestCase
+
+@end
+
+@implementation LostTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
